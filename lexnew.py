688eb98ab624caac4958e434d5e24f0df6d86189
Marcos Guimaraes <gallotti@geomagg.com>
2018-05-23 09:40:42 -0500

    Including quit

--- lexnew.py
+++ lexnew.py
@@ -13,10 +13,57 @@ import resources
 class MapExplorer(QMainWindow, Ui_ExplorerWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-#        self.setWindowTitle("Landmark Explorer")
-#        self.resize(1200, 400)
 
         self.setupUi(self)
+#######################
+
+        self.connect(self.actionQuit, SIGNAL("triggered()"),
+                     qApp.quit)
+        self.connect(self.actionShowBasemapLayer, SIGNAL("triggered()"),
+                     self.showBasemapLayer)
+        self.connect(self.actionShowLandmarkLayer, SIGNAL("triggered()"),
+                     self.showLandmarkLayer)
+        self.connect(self.actionZoomIn, SIGNAL("triggered()"),
+                     self.zoomIn)
+        self.connect(self.actionZoomOut, SIGNAL("triggered()"),
+                     self.zoomOut)
+
+        self.connect(self.actionPan, SIGNAL("triggered()"),
+                     self.setPanMode)
+        self.connect(self.actionExplore, SIGNAL("triggered()"),
+                     self.setExploreMode)
+
+    def showBasemapLayer(self):
+#        self.showVisibleMapLayers()
+     pass
+
+    def showLandmarkLayer(self):
+#        self.showVisibleMapLayers()
+     pass
+
+    def zoomIn(self):
+#        self.mapCanvas.zoomIn()
+     pass
+
+    def zoomOut(self):
+#        self.mapCanvas.zoomOut()
+     pass
+
+    def setPanMode(self):
+     pass
+#        self.actionPan.setChecked(True)
+#        self.actionExplore.setChecked(False)
+#        self.mapCanvas.setMapTool(self.panTool)
+
+
+    def setExploreMode(self):
+     pass
+#        self.actionPan.setChecked(False)
+#        self.actionExplore.setChecked(True)
+#        self.mapCanvas.setMapTool(self.exploreTool)
+
+
+
 
 #def main():
 
